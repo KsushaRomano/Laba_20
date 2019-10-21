d2e6d4d1a37f81545e84240748b734c152f7c0c0
KsushaRomano <55737387+KsushaRomano@users.noreply.github.com>
2019-10-21 19:20:00 +0300

    Add files via upload

---
+++ Laba_20_5.cpp
@@ -0,0 +1,21 @@
+﻿using namespace std;
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	string S, S0;
+	int l, i, k;
+	k = 0;
+	cout << "Введите первую строку: ";
+	getline(cin, S);
+	cout << "Введите вторую строку: ";
+	getline(cin, S0);
+	while ((S.find(S0) >= 0) & (S.find(S0) < (S.size() + 1))) {
+		S.erase(S.find(S0), S0.length());
+		k++;
+	};
+	cout << "Количество вхождений: " << k;
+}

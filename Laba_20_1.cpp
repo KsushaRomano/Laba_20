33e7b2ab83b250cdbb6b19faa7a5e3f0cb70805b
KsushaRomano <55737387+KsushaRomano@users.noreply.github.com>
2019-10-21 16:49:47 +0300

    Add files via upload

---
+++ Laba_20_1.cpp
@@ -0,0 +1,13 @@
+﻿using namespace std;
+
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	char C;
+	cout << "Введите символ: ";
+	cin >> C;
+	cout << "Предшедствующий символ: " << char(C - 1) << endl;
+	cout << "Следующий символ: " << char(C + 1) << endl;
+}

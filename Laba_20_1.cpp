eec6e2b709b9386f5e8054531fc4076e488beefc
KsushaRomano <55737387+KsushaRomano@users.noreply.github.com>
2019-10-21 23:17:31 +0300

    Update Laba_20_1.cpp

--- Laba_20_1.cpp
+++ Laba_20_1.cpp
@@ -1,10 +1,13 @@
 ﻿using namespace std;
 
 #include <iostream>
+#include <windows.h>
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 	char C;
 	cout << "Введите символ: ";
 	cin >> C;

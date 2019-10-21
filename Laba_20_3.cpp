1226d03bc2957886bfa9ede815453e591ada7ecd
KsushaRomano <55737387+KsushaRomano@users.noreply.github.com>
2019-10-21 17:30:39 +0300

    Add files via upload

---
+++ Laba_20_3.cpp
@@ -0,0 +1,21 @@
+﻿using namespace std;
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	string S;
+	int k, l, i;
+	k = 0;
+	cout << "Введите строку: ";
+	getline(cin, S);
+	l = S.size();
+	for (i = 0; i < l; i++) {
+		if (isupper(S[i])) {
+		k++;
+		};
+	};
+	cout << "В строке всего прописных латинскх букв: " << k;
+}

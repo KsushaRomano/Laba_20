bcf7004f6303c74b0ccbaa8c5410d7ea92c7610c
KsushaRomano <55737387+KsushaRomano@users.noreply.github.com>
2019-10-21 23:18:48 +0300

    Update Laba_20_4.cpp

--- Laba_20_4.cpp
+++ Laba_20_4.cpp
@@ -2,10 +2,13 @@ using namespace std;
 
 #include <iostream>
 #include <string>
+#include <windows.h>
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 	string S;
 	char C;
 	int i, l;
